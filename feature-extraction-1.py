74048f0e6a8201f1a4608f08d67398a276e65265
Michael Hypnarowski <mhypnaro@gmail.com>
2019-12-02 21:05:03 -0800

    Update feature-extraction-1.py

--- feature-extraction-1.py
+++ feature-extraction-1.py
@@ -7,42 +7,39 @@ def extract_all(instance):
     features = {}
 
     features["length"] = get_length(instance)
-    features["1-star-words"] = get_one_star_words(instance)
+    features["negative-words"] = get_negative_words(instance)
     features["useful"] = instance["useful"]
     features["funny"] = instance["funny"]
     features["cool"] = instance["cool"]
+
+    return features
 
 
 def get_length(instance):
     return len(instance["tokens"])
 
 
-def get_one_star_words(instance):
-    one_star_words = {"horrible"}
-    text_as_set = set(instance["tokens"])
-    res = text_as_set.intersection(one_star_words)
-    return len(res)
+def get_negative_words(instance):
+    negative_words = {"horrible"}
+    num_negative = 0
+    for token in instance["tokens"]:
+        if token in negative_words:
+            num_negative += 1
+    return num_negative
 
-
-
-#     return
-#
-# def 2_star_words(instance):
-#
-# def 3_star_words(instance):
-#
-# def 4_star_words(instance):
-#
-# def 5_star_words(intance):
 
 
 if __name__ == "__main__":
     test_instance = {
-        "text": ["Hello", ",", "world", "!"],
+        "tokens": ["horrible", "horrible", "world", "!"],
         "useful": 0,
         "funny": 1,
         "cool": 1
     }
+
+    result = extract_all(test_instance)
+
+    print(result)
 
     with open("data_train.json") as train_data:
         data = json.load(train_data)
@@ -56,9 +53,6 @@ if __name__ == "__main__":
 
 
         for i in data:
-            # tokens = i['text'].split()
-            # bigrams = [(tokens[j].lower(), tokens[j+1].lower()) for j in range(0,len(tokens)-1)]
-            # print(bigrams)
             if i["stars"] == 1:
                 result["1-star"].update(i["text"].split())
             elif i["stars"] == 2:
@@ -69,13 +63,6 @@ if __name__ == "__main__":
                 result["4-star"].update(i["text"].split())
             elif i["stars"] == 5:
                 result["5-star"].update(i["text"].split())
-
-        pprint.pprint(result["2-star"].most_common(500))
-
-
-
-
-
-    # result = extract_all(test_instance)
-    #
-    # print (result)
+                
+            pprint.pprint(result["3-star"])
+    
