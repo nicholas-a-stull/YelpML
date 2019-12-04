b4db2e5b404b4652ee51856ccfd14b0098710e9e
Abteen Ebrahimi <abteen.ebrahimi@gmail.com>
2019-12-03 20:34:26 -0800

    refactor pipeline

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # YelpML
+
+###Description of Files:
+
+    - bow.py: Bag-of-Words object to create a unigram and bigram vocabulary, and used to generate
+        bow features.
+    - pipeline.py: Runs the whole pipeline for classification, including preprocessing, creating vocabulary,
+       converting text features to vectors and running the model.
+    - feature_extractor.py: Handles feature extraction for dataframes
+    - skl_{nb,log}.py: Simple files to handle training and validation for their respective models
+    
+    
+###Notes on running and runtime:
+    - Loading the data and preprocessing the data takes a decent amount of time due to tokenization
+    - To save time, the pipeline pickles the preprocessed data, so if you are running the code twice,
+       uncomment/comment the correct lines to only run preprocessing once.
+    - Model selection is handled at the end of the pipeline, using one of the model objects.
+    
+    
+    
+    
+    
+Notes for writeup:
+
+Preprocessing:
+    While the data is structured, with information such as the cool, useful, and funny counts, we found that these
+    attributes were not helpful features in our models. As the only usable feature was the text field, we decided to preprocess 
+    the data in order to reduce noise present in the reviews. To do this, we lowercase all the text, strip whitespace and 
+    punctuation, as well as tokenize the text using NLTK. We then use NLTK's built-in corpora of stop words to remove anything
+    that has a low chance of being informative. Due to this preprocessing, the text attribute of each instance is converted 
+    to a bag of words representation covering the whole instance, and we lose sentence structure. 
+    
+    The next step in our preprocessing is creating a vocabulary of unigrams and bigrams generated from our dataset. 
+    We keep the 500 most frequent from each set, and create an indexed vocabulary, i.e. a dictionary mapping each token to a unique integer. 
+    The value of 500 is arbitary, and selected through trial and error.
+    This vocabulary is used in feature generation. Although we originally wanted to create a vocabulary conditioned on instance labels, we found that this method yielded better results.
+    
+Feature Creation:
+    To convert the bag of words for each instance into a structured vector, we use the generated dictionaries. Respectively for both unigrams and bigrams,
+    we create a 500 dimensional vector, which represents the counts present in the given instance.
+    Therefore, v_i = count(token) where vocabulary(token) = i.
+    The process for bigram features is identical. 
+    
+Model Usage:
+    Since our feature vectors and labels are all numpy arrays, we can easily use the models provided by scikit-learn. 
+    We found that these implementations had reasonable running time and were easy to experiment with. 
+    
+    Logistic Regression: Hyperparamter selction
--- pipeline2.py
+++ pipeline2.py
@@ -10,69 +10,113 @@ from skl_log import LogReg
 
 from sklearn.metrics import confusion_matrix
 
-cached_stopwords = stopwords.words('english')
+
+
+class Pipeline():
+
+    def __init__(self):
+        self.cached_stopwords = stopwords.words('english')
+        self.nb = None
+        self.log_reg = None
+
+    def preprocess(self,text):
+        # Lower case and strip whitespace
+        text = text.lower().strip()
+
+        # Strip punctuation
+        text = re.sub(r'[.,\-?!\'\"\$\%\#():]', '', text)
+
+        # Tokenize text
+        tokens = word_tokenize(text)
+
+        # Remove stop words
+        tokens = [token for token in tokens if token not in self.cached_stopwords]
+
+        return tokens
+
+    def load_data(self, file, validate=True):
+
+        fname = file[:-5]
+
+        # all_avail = pandas.read_json(open(file, 'r'))
+        # all_avail['text'] = all_avail['text'].apply(func=self.preprocess)
+        # pickle.dump(all_avail, open(fname + '.pickle', 'wb'))
 
 
 
-def preprocess(text):
+        all_avail = pickle.load(open(fname + '.pickle', 'rb'))
+        print('Finished preprocessing')
 
-    #Lower case and strip whitespace
-    text = text.lower().strip()
+        # 80-20 Split for training and development
+        if validate:
+            train_set = all_avail.sample(frac=0.8, random_state=1)
+            dev_set = all_avail.drop(train_set.index)
+        else:
+            train_set = all_avail
 
-    #Strip punctuation
-    text = re.sub(r'[.,\-?!\'\"\$\%\#():]', '', text)
+        # Instantiate bag-of-words object
+        bow = BOW(train_set)
 
-    #Tokenize text
-    tokens = word_tokenize(text)
+        # Create vocabulary
+        bow.create_bigram_vocabulary(500)
+        bow.create_vocabulary(500)
+        print('Finished BOW')
 
-    #Remove stop words
-    tokens = [token for token in tokens if token not in cached_stopwords]
+        feats = FeatureExtractor(bow)
+        train_X, train_label = feats.df_to_feats_skl(train_set)
+        if validate:
+            dev_X, dev_label = feats.df_to_feats_skl(dev_set)
+            return train_X, train_label, dev_X, dev_label
 
-    return tokens
+        else:
+            return train_X, train_label
+
+    def run_training(self, input_file, validate=True):
+
+        if validate:
+            train_X, train_label, dev_X, dev_label = self.load_data(input_file, validate)
+        else:
+            train_X, train_label = self.load_data(input_file, validate)
+            dev_X = None
+            dev_label = None
+
+        print('Finished converting instances to vectors')
+
+        #Train naive bayes and get its accuracy
+        # nb = NBModel(train_X, train_label, dev_X, dev_label)
+        # nb.train()
+        # self.nb = nb
+        # if validate:
+        #     print("Naive Bayes Model Accurracy:")
+        #     print(nb.validate())
+
+        # Train logistic regression and get its accurracy
+        log_reg = LogReg(train_X, train_label, dev_X, dev_label)
+        log_reg.train()
+        self.log_reg = log_reg
+        if validate:
+            print("\nLogistic Regression Model Accurracy: {}".format(log_reg.validate()))
+
+    def predict_file(self, test_file):
+
+        test_x, test_y = self.load_data(self, test_file, validate=False)
+
+        if self.nb:
+            raise NotImplementedError
+
+        if self.log_reg:
+            raise NotImplementedError
+
+
+
+
+
+
+def main():
+    pipe = Pipeline()
+    pipe.run_training('data_train.json', validate=True)
 
 if __name__ == '__main__':
-
-    # train_file = "data_train.json"
-    # all_avail = pandas.read_json(open(train_file, 'r'))
-    
-    # all_avail['text'] = all_avail['text'].apply(func=preprocess)
-    # pickle.dump(all_avail, open('all_avail.pickle', 'wb'))
-    all_avail = pickle.load(open('all_avail.pickle', 'rb'))
-    print('Finished preprocessing')
-
-    #80-20 Split for training and development
-    train_set = all_avail.sample(frac=0.8, random_state=1)
-    dev_set = all_avail.drop(train_set.index)
-
-    #Instantiate bag-of-words object
-    bow = BOW(train_set)
-
-    #Create vocabulary
-    bow.create_bigram_vocabulary(500)
-    bow.create_vocabulary(500)
-    print('Finished BOW')
-
-    feats = FeatureExtractor(bow)
-    train_X, train_label = feats.df_to_feats_skl(train_set)
-    dev_X, dev_label = feats.df_to_feats_skl(dev_set)
-
-    print(train_X)
-    print(train_label)
-    print('Finished converting instances to vectors')
-
-    # #Train naive bayes and get its accuracy
-    # nb = NBModel(train_X, train_label, dev_X, dev_label)
-    # nb.train()
-    # print("Naive Bayes Model Accurracy:")
-    # print(nb.validate())
-
-    #Train logistic regression and get its accurracy
-    log_reg = LogReg(train_X, train_label, dev_X, dev_label)
-    log_reg.train()
-    print("\nLogistic Regression Model Accurracy: {}".format(log_reg.validate()))
-
-
-
-
+    main()
 
 
