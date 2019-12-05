7e8fce983cf0b11dc56d4ff89dcfeb8060ba1bc2
Nicholas Stull <43829441+nicholas-a-stull@users.noreply.github.com>
2019-12-04 16:55:20 -0800

    added ptron

---
+++ skl_ptron.py
@@ -0,0 +1,18 @@
+from sklearn.linear_model import Perceptron
+
+class Ptron():
+
+    def __init__(self, tx, ty, dx, dy):
+
+        self.train_x = tx
+        self.train_y = ty
+        self.dev_x = dx
+        self.dev_y = dy
+
+        self.model = Perceptron()
+
+    def train(self):
+        self.model.fit(self.train_x, self.train_y)
+
+    def validate(self):
+        return self.model.score(self.dev_x, self.dev_y)
